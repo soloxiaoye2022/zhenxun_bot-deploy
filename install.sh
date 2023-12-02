f8110714b45a4ed076acf43a4241635f96f574e7
soloxiaoye2022 <soloxiaoye2022@163.com>
2023-12-02 08:21:18 +0800

    修复数据库未正常创建问题

--- install.sh
+++ install.sh
@@ -85,11 +85,11 @@ Set_pip_Mirror() {
   [[ ${mirror_num} == 3 ]] && mirror_url='"http://mirrors.aliyun.com/pypi/simple/"'
   [[ ${mirror_num} == 4 ]] && mirror_url='"https://pypi.mirrors.ustc.edu.cn/simple/"'
   [[ ${mirror_num} == 5 ]] && mirror_url='"https://mirrors.bfsu.edu.cn/pypi/web/simple/"'
-  if [ $mirror_num -ge 1 -a $mirror_num -le 5 ];then
+  if [ "$mirror_num" -ge 1 -a "$mirror_num" -le 5 ];then
      sed -i "s|url.*|url = "${mirror_url}"|g" ${WORK_DIR}/zhenxun_bot/pyproject.toml
      pip_url=$(echo $mirror_url | sed 's/\"//g')
      pip config set global.index-url "${pip_url}"
-  elif [ $mirror_num -gt 6 ]; then
+  elif [ "$mirror_num" -gt 6 ]; then
      echo -e"${Info} 你可能没有输入正确的选项?"
      Set_pip_Mirror
   fi
@@ -107,9 +107,9 @@ Installation_dependency() {
         yum -y update
         yum install -y git fontconfig mkfontscale epel-release wget vim curl zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gcc make libffi-devel
         if  ! which python3.8 && ! which python3.9; then
-            wget https://mirrors.huaweicloud.com/python/3.9.10/Python-3.9.10.tgz -O ${TMP_DIR}/Python-3.9.10.tgz && \
-                tar -zxf ${TMP_DIR}/Python-3.9.10.tgz -C ${TMP_DIR}/ &&\
-                cd ${TMP_DIR}/Python-3.9.10 --with-ensurepip=install && \
+            wget https://mirrors.huaweicloud.com/python/3.9.10/Python-3.9.10.tgz -O "${TMP_DIR}"/Python-3.9.10.tgz && \
+                tar -zxf "${TMP_DIR}"/Python-3.9.10.tgz -C "${TMP_DIR}"/ &&\
+                cd "${TMP_DIR}"/Python-3.9.10 --with-ensurepip=install && \
                 ./configure && \
                 make -j $(cat /proc/cpuinfo |grep "processor"|wc -l) && \
                 make altinstall
@@ -132,9 +132,9 @@ EOF
         apt-get update
         apt-get install -y wget ttf-wqy-zenhei xfonts-intl-chinese wqy* build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
         if  ! which python3.8 && ! which python3.9;then
-            wget https://mirrors.huaweicloud.com/python/3.9.10/Python-3.9.10.tgz -O ${TMP_DIR}/Python-3.9.10.tgz && \
-                tar -zxf ${TMP_DIR}/Python-3.9.10.tgz -C ${TMP_DIR}/ &&\
-                cd ${TMP_DIR}/Python-3.9.10 && \
+            wget https://mirrors.huaweicloud.com/python/3.9.10/Python-3.9.10.tgz -O "${TMP_DIR}"/Python-3.9.10.tgz && \
+                tar -zxf "${TMP_DIR}"/Python-3.9.10.tgz -C "${TMP_DIR}"/ &&\
+                cd "${TMP_DIR}"/Python-3.9.10 && \
                 ./configure --with-ensurepip=install && \
                 make -j $(cat /proc/cpuinfo |grep "processor"|wc -l) && \
                 make altinstall
@@ -217,17 +217,17 @@ log="/root/zhenxun_bot/zhenxun_bot.log"
 dir="/root/zhenxun_bot"
 echo -e "${Info} 自动检测真寻确实依赖并自动安装"
 if [[ -e "${log}" ]]; then
-[[ ! -z ${PID} ]] && kill -9 ${PID}
-cd ${dir}
+[[ ! -z ${PID} ]] && kill -9 "${PID}"
+cd ${dir} || exit
 nohup poetry run ${python_v} bot.py > zhenxun_bot.log 2>&1 & 
 echo -e "${Info} 正在检测 zhenxun_bot 缺失依赖(需要约1分钟)"
 sleep 60
-[[ ! -z ${PID} ]] && kill -9 ${PID}
+[[ ! -z ${PID} ]] && kill -9 "${PID}"
 cat ${log} | grep "No module named" | awk -F "'" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' | sed 's/ /\n/' > /root/zhenxun_bot/requests.txt
 Install_module
 else
 echo -e "${Error} log文件不存在,即将自动启动 zhenxun_bot 并生成log文件"
-cd ${dir}
+cd ${dir} || exit
 nohup poetry run ${python_v} bot.py > zhenxun_bot.log 2>&1 & 
 echo -e "${Info} log文件已生成"
 sleep 5
@@ -238,12 +238,12 @@ fi
 
 Install_module() {
 module=$(sort -u /root/zhenxun_bot/requests.txt | uniq | sed '/^enchant/d')
-echo ${module} > /root/zhenxun_bot/requests.txt
+echo "${module}" > /root/zhenxun_bot/requests.txt
 if [[ ! -z "${module}" ]]; then
-cd ${dir}
+cd ${dir} || exit
 echo -e "${Info} 本次检测到以下版本依赖需要安装,如果安装失败请手动进入虚拟环境执行poetry install"
 sleep 1
-poetry run pip install $module -i ${pip_url}
+poetry run pip install "$module" -i "${pip_url}"
 echo -e "${Info} 本次依赖安装结束"
 check_module
 else
@@ -321,7 +321,7 @@ Set_config_admin() {
     read -erp "管理员QQ:" admin_qq
     [[ -z "$admin_qq" ]] && admin_qq=""
     cd ${WORK_DIR}/zhenxun_bot && sed -i "s/SUPERUSERS.*/SUPERUSERS=[\"$admin_qq\"]/g" .env.dev || echo -e "${Error} 配置文件不存在！请检查zhenxun_bot是否安装正确!"
-    echo -e "${Info} 设置成功!管理员QQ: ["${Green_font_prefix}${admin_qq}${Font_color_suffix}"]"
+    echo -e "${Info} 设置成功!管理员QQ: [""${Green_font_prefix}"${admin_qq}"${Font_color_suffix}""]"
     
 }
 
@@ -329,12 +329,12 @@ Set_config_bot() {
     echo -e "${Info} 请输入Bot QQ账号:[QQ]"
     read -erp "Bot QQ:" bot_qq
     [[ -z "$bot_qq" ]] && bot_qq=""
-    if [ $bot_qq = $admin_qq ]; then
-      echo -e "${Error} Bot QQ["${Green_font_prefix}$bot_qq${Font_color_suffix}"]不能与管理员QQ账号["${Green_font_prefix}$admin_qq${Font_color_suffix}"]一致"
+    if [ "$bot_qq" = "$admin_qq" ]; then
+      echo -e "${Error} Bot QQ[""${Green_font_prefix}"$bot_qq"${Font_color_suffix}""]不能与管理员QQ账号[""${Green_font_prefix}"$admin_qq"${Font_color_suffix}""]一致"
       Set_config_bot
     else
       cd ${WORK_DIR}/go-cqhttp && sed -i "s/uin:.*/uin: $bot_qq # QQ账号/g" config.yml || echo -e "${Error} 配置文件不存在！请检查go-cqhttp是否安装正确!"
-      echo -e "${Info} 设置成功!Bot QQ: ["${Green_font_prefix}${bot_qq}${Font_color_suffix}"]"
+      echo -e "${Info} 设置成功!Bot QQ: [""${Green_font_prefix}"${bot_qq}"${Font_color_suffix}""]"
     fi
     echo -e "${Info} 请选择Bot 登陆方式(默认为扫码登陆)"
     read -erp "请选择 [1-2], 默认为 1:" login_mode
@@ -344,14 +344,14 @@ Set_config_bot() {
         while true; do
             read -erp "Bot Password:" bot_password
             read -erp "${Info} 请再次输入密码:" again_password
-            if [ $again_password != $bot_password ]; then
+            if [ "$again_password" != "$bot_password" ]; then
                 echo -e "${Error} 两次输入的密码不一致，请重新输入!"
             else
                 break
             fi
         done
         cd ${WORK_DIR}/go-cqhttp && sed -i "s/password:.*/password: '$bot_password' # 密码为空时使用扫码登录/g" config.yml || echo -e "${Error} 配置文件不存在！请检查go-cqhttp是否安装正确!"
-        echo -e "${Info} 设置成功!Bot Password: ["${Green_font_prefix}${bot_password}${Font_color_suffix}"]"
+        echo -e "${Info} 设置成功!Bot Password: [""${Green_font_prefix}"${bot_password}"${Font_color_suffix}""]"
     fi
 }
 
@@ -369,14 +369,14 @@ Set_config() {
 }
 
 Set_Port() {
-echo -e "${Info} 请设置zhenxun_bot gocq通信端口:取值范围["${Green_font_prefix}${mix}-${max}${Font_color_suffix}"]"
+echo -e "${Info} 请设置zhenxun_bot gocq通信端口:取值范围[""${Green_font_prefix}"${mix}-${max}"${Font_color_suffix}""]"
     read -erp "Port:" Port
     [[ -z "${Port}" ]] && Port=""
-      if [ ${Port} -ge ${mix} -a ${Port} -le ${max} ]; then
+      if [ "${Port}" -ge ${mix} -a "${Port}" -le ${max} ]; then
         cd ${WORK_DIR}/zhenxun_bot  && sed -i "s|PORT.*|PORT = ${Port}|g" .env.dev && cd ${WORK_DIR}/go-cqhttp && sudo sed -i "s|universal:.*|universal: ws://127.0.0.1:${Port}/onebot/v11/ws|g" config.yml || (echo -e "${Error} 配置文件不存在！请检查 zhenxun_bot 或 gocq是否安装正确!" && exit 1)
-    echo -e "${Info} 设置成功!端口: ["${Green_font_prefix}${Port}${Font_color_suffix}"]"
+    echo -e "${Info} 设置成功!端口: [""${Green_font_prefix}"${Port}"${Font_color_suffix}""]"
       else 
-      echo -e "${Error} 端口设置错误，取值范围["${Green_font_prefix}${mix}-${max}${Font_color_suffix}"]"
+      echo -e "${Error} 端口设置错误，取值范围[""${Green_font_prefix}"${mix}-${max}"${Font_color_suffix}""]"
       Set_Port
      fi     
 }
@@ -390,7 +390,7 @@ Start_zhenxun_bot() {
     check_pid_zhenxun
     [[ -n ${PID} ]] && echo -e "${Error} zhenxun_bot 正在运行，请检查 !" && exit 1
     Start_postgresql
-    cd ${WORK_DIR}/zhenxun_bot
+    cd ${WORK_DIR}/zhenxun_bot || exit
     nohup poetry run python3 bot.py >> zhenxun_bot.log 2>&1 &
     echo -e "${Info} zhenxun_bot 开始运行..."
 }
@@ -399,7 +399,7 @@ Stop_zhenxun_bot() {
     check_installed_zhenxun_status
     check_pid_zhenxun
     [[ -z ${PID} ]] && echo -e "${Error} zhenxun_bot 没有运行，请检查 !" && exit 1
-    kill -9 ${PID}
+    kill -9 "${PID}"
     echo -e "${Info} zhenxun_bot 已停止运行..."
 }
 
@@ -413,22 +413,22 @@ View_zhenxun_log() {
 }
 
 Set_config_zhenxun() {
-    vim ${WORK_DIR}/zhenxun_bot/configs/config.yaml
+    vim "${WORK_DIR}"/zhenxun_bot/configs/config.yaml
 }
 
 Start_cqhttp() {
     check_installed_cqhttp_status
     check_pid_cqhttp
     [[ -n ${PID} ]] && echo -e "${Error} go-cqhttp 正在运行，请检查 !" && exit 1
-    cd ${WORK_DIR}/go-cqhttp
+    cd ${WORK_DIR}/go-cqhttp || exit
     nohup ./go-cqhttp -faststart >> go-cqhttp.log 2>&1 &
     echo -e "${Info} go-cqhttp 开始运行..."
     sleep 2
 }
 
 Start_postgresql() {
-INODE_NUM=`ls -ali / | sed '2!d' |awk {'print $1'}`
-if [ $INODE_NUM == '2' ]; then
+INODE_NUM=$(ls -ali / | sed '2!d' |awk {'print $1'})
+if [ "$INODE_NUM" == '2' ]; then
 systemctl start postgresql
 else
 su postgres <<-EOF
@@ -443,8 +443,8 @@ echo -e "${Info} pgsql数据库已重启"
 }
 
 Stop_postgresql() {
-INODE_NUM=`ls -ali / | sed '2!d' |awk {'print $1'}`
-if [ $INODE_NUM == '2' ]; then
+INODE_NUM=$(ls -ali / | sed '2!d' |awk {'print $1'})
+if [ "$INODE_NUM" == '2' ]; then
 systemctl stop postgresql
 else
 su postgres <<-EOF
@@ -464,7 +464,7 @@ Stop_cqhttp() {
     check_installed_cqhttp_status
     check_pid_cqhttp
     [[ -z ${PID} ]] && echo -e "${Error} cqhttp 没有运行，请检查 !" && exit 1
-    kill -9 ${PID}
+    kill -9 "${PID}"
     echo -e "${Info} go-cqhttp 停止运行..."
 }
 
@@ -558,7 +558,7 @@ read -erp "请输入镜像站地址(示例https://mirrors.bfsu.edu.cn/):" input
    echo -e "${Info} 请输入正确的镜像站地址"
    Manual_input_source
   else
-     symbol=`echo ${input: -1}`
+     symbol=$(echo ${input: -1})
      character="/"
     if [ "$sybmol" = "character"  ];
      then
@@ -571,7 +571,7 @@ fi
 }
 
 Exit_cqhttp() {
-    cd ${WORK_DIR}/go-cqhttp
+    cd ${WORK_DIR}/go-cqhttp || exit
     rm -f session.token
     echo -e "${Info} go-cqhttp 账号已退出..."
     Stop_cqhttp
@@ -580,7 +580,7 @@ Exit_cqhttp() {
 }
 
 Set_dependency() {
-    cd ${WORK_DIR}/zhenxun_bot
+    cd ${WORK_DIR}/zhenxun_bot || exit
     Set_pip_Mirror
     ${python_v} -m pip install poetry
     poetry env use ${python_v}
@@ -591,13 +591,13 @@ Set_dependency() {
 }
 
 Install_sshd() {
-INODE_NUM=`ls -ali / | sed '2!d' |awk {'print $1'}`
-if [ ! $INODE_NUM == '2' ]; then
+INODE_NUM=$(ls -ali / | sed '2!d' |awk {'print $1'})
+if [ ! "$INODE_NUM" == '2' ]; then
     echo -e "${Info} 开始安装ssh服务"
         apt install ssh -y
         echo -e "${Info} 安装完成,检查ssh配置文件"
         wget https://gitee.com/soloxiaoye/zhenxun_bot_tool/attach_files/1095259/download/sshd_config -qO /etc/ssh/sshd_config
-        echo -e "${Info} 请设置ssh端口，取值范围["${Green_font_prefix}${mix}-${max}${Font_color_suffix}"]"
+        echo -e "${Info} 请设置ssh端口，取值范围[""${Green_font_prefix}"${mix}-${max}"${Font_color_suffix}""]"
    Set_hhd_port
 fi
 }
@@ -606,13 +606,13 @@ fi
 Set_hhd_port() {
 read -erp "Port:" num
     [[ -z "$num" ]] && num="${ssh_port}"
-    if [ ${num} -ge ${mix} -a ${num} -le ${max} ]; then
+    if [ "${num}" -ge ${mix} -a "${num}" -le ${max} ]; then
     cd /etc/ssh/ && sed -i "s|Port.*|Port ${num}|g" sshd_config || echo -e "${Error} 配置文件不存在！请检查ssh是否安装正确!"
-    echo -e "${Info} 设置成功!端口: ["${Green_font_prefix}${num}${Font_color_suffix}"]"
+    echo -e "${Info} 设置成功!端口: [""${Green_font_prefix}"${num}"${Font_color_suffix}""]"
         echo -e "${Info} 启动ssh远程连接服务"
         /etc/init.d/ssh restart
     else 
-      echo -e "${Error} 端口设置错误，取值范围["${Green_font_prefix}${mix}-${max}${Font_color_suffix}"]"
+      echo -e "${Error} 端口设置错误，取值范围[""${Green_font_prefix}"${mix}-${max}"${Font_color_suffix}""]"
       Set_hhd_port
     fi
 
@@ -623,13 +623,13 @@ Uninstall_All() {
   read -erp "请选择 [y/n], 默认为 n:" uninstall_check
   [[ -z "${uninstall_check}" ]] && uninstall_check='n'
   if [[ ${uninstall_check} == 'y' ]]; then
-    cd ${WORK_DIR}
+    cd ${WORK_DIR} || exit
     check_pid_zhenxun
-    [[ -z ${PID} ]] || kill -9 ${PID}
+    [[ -z ${PID} ]] || kill -9 "${PID}"
     echo -e "${Info} 开始卸载 zhenxun_bot..."
     rm -rf zhenxun_bot || echo -e "${Error} zhenxun_bot 卸载失败！"
     check_pid_cqhttp
-    [[ -z ${PID} ]] || kill -9 ${PID}
+    [[ -z ${PID} ]] || kill -9 "${PID}"
     echo -e "${Info} 开始卸载 go-cqhttp..."
     rm -rf go-cqhttp || echo -e "${Error} go-cqhttp 卸载失败！"
     echo -e "${Info} 感谢使用真寻bot，期待于你的下次相会！"
@@ -640,7 +640,7 @@ Uninstall_All() {
 Install_zhenxun_bot() {
     check_root
     [[ -e "${WORK_DIR}/zhenxun_bot/bot.py" ]] && echo -e "${Error} 检测到 zhenxun_bot 已安装 !" && exit 1
-    startTime=`date +%s`
+    startTime=$(date +%s)
     Set_ghproxy
     Set_apt_source
     echo -e "${Info} 开始检查系统..."
@@ -661,7 +661,7 @@ Install_zhenxun_bot() {
         cp -r ${WORK_DIR}/zhenxun_bot/resources/font /usr/share/fonts/chinese
         cd /usr/share/fonts/chinese && mkfontscale
     fi
-    endTime=`date +%s`
+    endTime=$(date +%s)
     ((outTime=($endTime-$startTime)))
     echo -e "${Info} 安装用时 ${outTime} s ..."
     Start_zhenxun_bot
@@ -672,8 +672,8 @@ Install_zhenxun_bot() {
 }
 
 Install_postgresql() {
-INODE_NUM=`ls -ali / | sed '2!d' |awk {'print $1'}`
-if [ $INODE_NUM == '2' ];
+INODE_NUM=$(ls -ali / | sed '2!d' |awk {'print $1'})
+if [ "$INODE_NUM" == '2' ];
 then
       echo -e "${Info} 开始安装pgsql数据库"
       apt-get install postgresql postgresql-contrib -y
@@ -703,37 +703,65 @@ check_pgsql
 fi
 }
 
-check_pgsql(){
-databaseuser="zhenxun"
-result=`su postgres << EOF
-echo -e "SELECT u.usename FROM pg_catalog.pg_user u  where u.usename='${databaseuser}';\n" | psql 
-EOF`
-usename=`echo $result | rev | awk -F' ' '{print $2}' | cut -b 1`
-dbname=`su postgres << EOF
-psql -l | grep $databaseuser | wc -l
-EOF`
-  if [ "$usename" -eq "0" -a "$dbname" -eq "0" ]; then
-  su postgres <<-EOF
-echo -e "CREATE USER zhenxun WITH PASSWORD 'zxpassword';\n CREATE DATABASE zhenxun OWNER zhenxun;\n" | psql
-EOF
-   echo -e "${Info} 创建数据库成功。用户名 $databaseuser\n数据库名 $databaseuser\n密码zxpassword"
- else
-  echo -e "${Tip} 用户名 ${databaseuser} 或者 数据库 ${databaseuser}已存在"
-fi
-echo -e "${Info} pgsql数据库安装完成"
-}
+#check_pgsql() {
+#databaseuser="zhenxun"
+#result=$(su postgres << EOF
+#echo -e "SELECT u.usename FROM pg_catalog.pg_user u  where u.usename='${databaseuser}';\n" | psql 
+#EOF)
+#usename=$(echo "$result" | rev | awk -F' ' '{print $2}' | cut -b 1)
+#dbname=$(su postgres << EOF
+#psql -l | grep $databaseuser | wc -l
+#EOF)
+#  if [ "$usename" -eq "0" -a "$dbname" -eq "0" ]; then
+#  su postgres <<-EOF
+#echo -e "CREATE USER zhenxun WITH PASSWORD 'zxpassword';\n CREATE DATABASE zhenxun OWNER zhenxun;\n" | psql
+#EOF
+#   echo -e "${Info} 创建数据库成功。用户名 $databaseuser\n数据库名 $databaseuser\n密码zxpassword"
+# else
+#  echo -e "${Tip} 用户名 ${databaseuser} 或者 数据库 ${databaseuser}已存在"
+#fi
+#
+#echo -e "${Info} pgsql数据库安装完成"
+#
+#}
 
+check_pgsql() {
+    databaseuser="zhenxun"
+    result=$(su postgres << EOF
+        echo 'SELECT u.usename FROM pg_catalog.pg_user u WHERE u.usename='"'${databaseuser}';"
+        psql
+EOF
+    )
+    usename=$(echo "$result" | rev | awk -F' ' '{print $2}' | cut -b 1)
+    dbname=$(su postgres << EOF
+        psql -l | grep $databaseuser | wc -l
+EOF
+    )
+    if [ "$usename" -eq "0" -a "$dbname" -eq "0" ]; then
+        su postgres <<-EOF
+            echo 'CREATE USER zhenxun WITH PASSWORD 'zxpassword';'
+            echo 'CREATE DATABASE zhenxun OWNER zhenxun;'
+            psql
+EOF
+        echo -e "${Info} 创建数据库成功。用户名 $databaseuser
+数据库名 $databaseuser
+密码zxpassword"
+    else
+        echo -e "${Tip} 用户名 ${databaseuser} 或者 数据库 ${databaseuser}已存在"
+    fi
+    echo -e "${Info} pgsql数据库安装完成"
+}
 
 Update_Shell(){
     echo -e "${Info} 开始更新install.sh"
     bak_dir_name="sh_bak/"
-    bak_file_name="${bak_dir_name}install.`date +%Y%m%d%H%M%s`.sh"
+    bak_file_name="${bak_dir_name}install.$(date +%Y%m%d%H%M%s).sh"
     if [[ ! -d ${bak_dir_name} ]]; then
         sudo mkdir -p ${bak_dir_name}
         echo -e "${Info} 创建备份文件夹${bak_dir_name}"
     fi
     wget ${update_shell_url} -O install.sh.new
-    cp -f install.sh ${bak_file_name}
+    cp -f install.sh "${bak_file_name}"
     echo -e "${Info} 备份原install.sh为${bak_file_name}"
     mv -f install.sh.new install.sh
     echo -e "${Info} install.sh更新完成，请重新启动"
